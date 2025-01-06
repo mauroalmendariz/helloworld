ea11551b3f60e4036f4e08d7af2201825306bb71
Mauro <mauro.almendariz@gmail.com>
2025-01-06 11:38:45 -0800

    updated the loops.py file with for and else

--- 09-loops.py
+++ 09-loops.py
@@ -8,3 +8,28 @@ count = 0
 while count < 5:
     print(count)
     count += 1
+
+#while loop printing 1-5 and then "break" out
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+
+#for loop that skips even numbers and only print odds. 
+count = 0
+for x in range(10):
+    # Check if x is even.
+    if x % 2 == 0:
+        continue
+    print(x)
+
+# Else in a for loop.
+count = 0
+for x in range(5):
+    # Check if x is odd.
+    if x % 2 == 0:
+        print(x)
+    else:
+        print("This is supposed to be an odd number.")
